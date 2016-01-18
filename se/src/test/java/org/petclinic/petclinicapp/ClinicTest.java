cc4af4bc0b3d6ac12c7aac1920983bedf64d0167
dmitriy <FFKL@yandex.ru>
2016-01-19 01:01:58 +0200

    test 92% coverage

--- se/src/test/java/org/petclinic/petclinicapp/ClinicTest.java
+++ se/src/test/java/org/petclinic/petclinicapp/ClinicTest.java
@@ -1,5 +1,6 @@
 package org.petclinic.petclinicapp;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.petclinic.petclinicapp.Exceptions.IDException;
 import org.petclinic.petclinicapp.Exceptions.WrongInputException;
@@ -12,51 +13,105 @@ import static org.junit.Assert.*;
 
 public class ClinicTest {
 
-    /*Clinic myClinic = new Clinic();
+    Clinic myClinic = new Clinic();
+
+    @Before
+    public void setUp() throws Exception {
+        myClinic.addClient(1, "Vasya");
+        myClinic.addPetForClient(1, "Cat", "Pushok");
+        myClinic.addClient(2, "Fedor");
+        myClinic.addPetForClient(2, "Cat", "Pushok");
+        myClinic.addPetForClient(2, "Dog", "Bull");
+        myClinic.addClient(3, "Grisha");
+    }
 
     @Test
-    public void testFindClientsByPetName() throws Exception {
-        List<Client> clients = new ArrayList<Client>();
+    public void testAddClient() throws Exception {
+        assertFalse(myClinic.isEmpty());
+    }
 
+    @Test
+    public void testAddPetForClient() throws Exception {
+        assertFalse(myClinic.findById(1).getPets().isEmpty());
+    }
 
-        assertEquals(clients, myClinic.findClientsByPetName("Cat"));
+    @Test
+    public void testFindClientsByPetName() throws  Exception {
+        List<Client> clients = new ArrayList<>();
+        Client client1 = new Client(1, "Vasya");
+        client1.addPet("Cat", "Pushok");
+        Client client2 = new Client(2, "Fedor");
+        client2.addPet("Cat", "Pushok");
+        client2.addPet("Dog", "Bull");
+        clients.add(client1);
+        clients.add(client2);
+
+        assertEquals(myClinic.findClientsByPetName("Pushok"), clients);
     }
 
     @Test
     public void testFindByClientName() throws Exception {
-        Clinic testClinic = new Clinic();
-
-
-        assertEquals(testClinic.findByClientName("Vasiliy"), myClinic.findByClientName("Vasiliy"));
+        List<Client> clients = new ArrayList<>();
+        Client client2 = new Client(2, "Fedor");
+        client2.addPet("Cat", "Pushok");
+        client2.addPet("Dog", "Bull");
+        clients.add(client2);
+        assertEquals(myClinic.findByClientName("Fedor"), clients);
     }
 
-    @Test(expected = WrongInputException.class)
-    public void testAddClientWIException() throws WrongInputException, IDException {
-        Clinic myClinic = new Clinic();
+    @Test
+    public void testFindById() throws Exception {
+        Client client2 = new Client(2, "Fedor");
+        client2.addPet("Cat", "Pushok");
+        client2.addPet("Dog", "Bull");
+
+        assertEquals(myClinic.findById(2),client2);
     }
 
-    @Test(expected = IDException.class)
-    public void testAddClientIDException() {
-        Clinic myClinic = new Clinic();
+    @Test
+    public void testChangeClientName() throws Exception {
+        myClinic.changeClientName(1, "Fedor");
+
+        assertEquals(myClinic.findById(1).getClientName(), "Fedor");
     }
 
-    @Test(expected = WrongInputException.class)
-    public void testFindClientsByPetName1() throws WrongInputException {
-        myClinic.findClientsByPetName("132Pushok");
+    @Test
+    public void testChangePetName() throws Exception {
+        myClinic.changePetName(2,"Pushok", "Snezhok");
+
+        assertEquals(myClinic.findById(2).getPets().get(0).getName(), "Snezhok");
     }
 
-    @Test(expected = WrongInputException.class)
-    public void testFindByClientNameEx() throws WrongInputException {
-        myClinic.findByClientName("123Vasiliy");
+    @Test
+    public void testRemovePet() throws Exception {
+        assertEquals(myClinic.findById(2).getPets().size(), 2);
+        myClinic.removePet(2, "Bull");
+        assertEquals(myClinic.findById(2).getPets().size(), 1);
     }
 
-    @Test(expected = WrongInputException.class)
-    public void testChangeClientNameEx() throws WrongInputException, IDException {
-        myClinic.changeClientName(2, "123Vasiliy");
+    @Test
+    public void testRemoveClient() throws Exception {
+        assertEquals(myClinic.getClients().size(), 3);
+        myClinic.removeClient(3);
+        assertEquals(myClinic.getClients().size(), 2);
     }
 
-    @Test(expected = WrongInputException.class)
-    public void testChangePetName() throws WrongInputException, IDException {
-        myClinic.changePetName(3, "Pushok32");
-    }*/
+    @Test
+    public void testGetClients() throws Exception {
+        List<Client> clients = new ArrayList<>();
+        clients.add(new Client(1, "Vasya"));
+        clients.get(0).addPet("Cat", "Pushok");
+        clients.add(new Client(2, "Fedor"));
+        clients.get(1).addPet("Cat", "Pushok");
+        clients.get(1).addPet("Dog", "Bull");
+        clients.add(new Client(3, "Grisha"));
+
+        assertEquals(myClinic.getClients(), clients);
+    }
+
+    @Test
+    public void testIsEmpty() throws Exception {
+        myClinic.removeAll();
+        assertTrue(myClinic.isEmpty());
+    }
 }
